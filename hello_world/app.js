52bedc971caf54adccd75e5181c7b20b229de5aa
Герасименко Ярослав <90767809+CorvusSharp@users.noreply.github.com>
2024-09-24 22:14:33 +0300

    Update app.js

--- hello_world/app.js
+++ hello_world/app.js
@@ -5,21 +5,33 @@ const app = express();
 const port = 3000;
 
 const db = new Database({
-  url: 'http://arango:8529', // имя сервиса в docker-compose
+  url: 'http://arango:8529', // Имя сервиса в docker-compose
   auth: { username: 'root', password: 'password' }
 });
 
-// Маршрут для проверки базы данных и вывода "Hello World"
+// Маршрут для проверки наличия коллекции и вывода "Hello World"
 app.get('/', async (req, res) => {
   try {
     await db.version();
-    res.send('Hello World');
+
+    const collectionName = 'citywalls';
+
+    const collection = db.collection(collectionName);
+
+    const exists = await collection.exists();
+
+    if (exists) {
+      res.send('Hello World');
+    } else {
+      res.status(404).send(`Коллекция "${collectionName}" не найдена.`);
+    }
+
   } catch (err) {
     console.error('Ошибка подключения к базе данных:', err);
-    res.status(500).send('Не удалось подключиться к базе данных');
+    res.status(500).send('Не удалось подключиться к базе данных.');
   }
 });
 
 app.listen(port, () => {
-  console.log(`Сервер запущен на порту ${port}`);
+  console.log(`Сервер запущен на http://localhost:${port}`);
 });
