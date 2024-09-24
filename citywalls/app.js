536b9c316d370d0fa5bdb5be00b3ce55a8f94d62
ardnaxelas <aardnaxelass@gmail.com>
2024-09-24 21:34:38 +0300

    example done

---
+++ citywalls/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { Database } = require('arangojs');
+
+const app = express();
+const port = 3000;
+
+const db = new Database({
+  url: 'http://arango:8529', // то имя сервиса в docker-compose
+  auth: { username: 'root', password: 'password' }
+});
+
+// Маршрут для проверки базы данных и вывода "Hello World"
+app.get('/', async (req, res) => {
+  try {
+    await db.version();
+    res.send('Hello World');
+  } catch (err) {
+    console.error('Ошибка подключения к базе данных:', err);
+    res.status(500).send('Не удалось подключиться к базе данных');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Сервер запущен на порту ${port}`);
+});
